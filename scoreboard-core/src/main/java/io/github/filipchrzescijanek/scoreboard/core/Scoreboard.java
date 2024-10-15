685502dcaf24f57ed913a9bbd5adb8168252b26b
Filip Chrześcijanek <filip.chrzescijanek@gmail.com>
2024-10-15 04:36:17 +0200

    Updated README with comments (#8)

--- scoreboard-core/src/main/java/io/github/filipchrzescijanek/scoreboard/core/Scoreboard.java
+++ scoreboard-core/src/main/java/io/github/filipchrzescijanek/scoreboard/core/Scoreboard.java
@@ -5,7 +5,6 @@ import io.github.filipchrzescijanek.scoreboard.domain.Score;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import io.github.filipchrzescijanek.scoreboard.commands.AddMatch;
